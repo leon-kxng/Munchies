2f219cf1754b530600647d91c9c42f49d2046884
masud520 <masud.abdi@student.moringaschool.com>
2023-11-21 16:23:31 +0300

    done with login feature

--- project/src/App.js
+++ project/src/App.js
@@ -1,36 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-import React, { useState } from 'react';
-import Login from './Login';
-
+import React from 'react'
+import Login from './Components/Logins/Login';
 const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [userEmail, setUserEmail] = useState('');
-
-  const handleLogin = (email) => {
-    setLoggedIn(true);
-    setUserEmail(email);
-  };
-
-  const handleLogout = () => {
-    setLoggedIn(false);
-    setUserEmail('');
-  };
-
   return (
-    <div className="App">
-      {!loggedIn ? (
-        <Login handleLogin={handleLogin} />
-      ) : (
-        <div>
-          <h1>Welcome, {userEmail}!</h1>
-          <button onClick={handleLogout}>Logout</button>
-          {/* Other components or content for logged-in users */}
-        </div>
-      )}
+    <div>
+      <Login/>
     </div>
-  );
-};
-
+  )
+}
 export default App;
