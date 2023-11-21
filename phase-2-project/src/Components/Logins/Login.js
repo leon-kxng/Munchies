dadfa445766bcfd26aabe4b06a5e7836c1785c6d
masud520 <masud.abdi@student.moringaschool.com>
2023-11-21 18:06:32 +0300

    done

---
+++ phase-2-project/src/Components/Logins/Login.js
@@ -0,0 +1,47 @@
+import './Login.css';
+import profile from "../Images/a.png";
+import email from "../Images/email.jpg";
+import pass from "../Images/pass.png";
+const Login=()=> {
+  return (
+    <div className="main">
+     <div className="sub-main">
+       <div>
+         <div className="imgs">
+           <div className="container-image">
+             <img src={profile}alt="profile" className="profile"/>
+
+           </div>
+
+
+         </div>
+         <div>
+           <h1>Login Page</h1>
+           <div>
+             <img src={email}alt="email" className="email"/>
+             <input type="text" placeholder="user name" className="name"/>
+           </div>
+           <div className="second-input">
+             <img src={pass} alt="pass" className="email"/>
+             <input type="password" placeholder="user name" className="name"/>
+           </div>
+          <div className="login-button">
+          <button>Login</button>
+          </div>
+
+            <p className="link">
+              <a href="#">Forgot password </a> <br/>
+              <a href="#">Sign Up</a>
+            </p>
+
+
+         </div>
+       </div>
+
+
+     </div>
+    </div>
+  );
+}
+
+export default Login;
