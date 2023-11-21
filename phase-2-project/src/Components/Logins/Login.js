7caaf210f43ad594d82be28885111064091fc907
masud520 <masud.abdi@student.moringaschool.com>
2023-11-21 18:19:30 +0300

    done

--- phase-2-project/src/Components/Logins/Login.js
+++ phase-2-project/src/Components/Logins/Login.js
@@ -19,19 +19,19 @@ const Login=()=> {
            <h1>Login Page</h1>
            <div>
              <img src={email}alt="email" className="email"/>
-             <input type="text" placeholder="user name" className="name"/>
+             <input type="text" placeholder="user name/email/phone no" className="name"/>
            </div>
            <div className="second-input">
              <img src={pass} alt="pass" className="email"/>
-             <input type="password" placeholder="user name" className="name"/>
+             <input type="password" placeholder="password" className="name"/>
            </div>
           <div className="login-button">
           <button>Login</button>
           </div>
 
             <p className="link">
-              <a href="#">Forgot password </a> <br/>
-              <a href="#">Sign Up</a>
+              <a href="#">Forgot password </a> <br/><br/>
+              <a href="#">SIGN UP</a>
             </p>
 
 
